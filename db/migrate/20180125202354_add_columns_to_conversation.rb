251e367b02481691b8917643f7d1bff92d4dae30
Sergey <sergeyripchanskiy@gmail.com>
2018-01-25 22:30:10 +0200

    add_columns_to_conversation model

---
+++ db/migrate/20180125202354_add_columns_to_conversation.rb
@@ -0,0 +1,9 @@
+class AddColumnsToConversation < ActiveRecord::Migration[5.1]
+  def change
+    add_column :conversations, :recipient_id, :integer
+    add_index :conversations, :recipient_id
+    add_column :conversations, :sender_id, :integer
+    add_index :conversations, :sender_id
+    add_index :conversations, [:recipient_id, :sender_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125201940) do
+ActiveRecord::Schema.define(version: 20180125202354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180125201940) do
   create_table "conversations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "messages", force: :cascade do |t|
