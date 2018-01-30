1ddfbff2d1e15ced38440b347ae82de2b0efb6d7
Sergey <sergeyripchanskiy@gmail.com>
2018-01-30 14:53:08 +0200

    add association has_many :messages to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :messages, dependent: :destroy
+
   validates :name, presence: :true
   validates :surname, presence: :true
   validates :age, presence: :true
