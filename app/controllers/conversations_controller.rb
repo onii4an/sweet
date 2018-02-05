e3ed6150e3e3ec8f76f8997d57706e9e170bf3d1
onii4an <svyat4work@gmail.com>
2018-02-05 19:17:56 +0200

    create conversations controller & page

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,27 @@
+class ConversationsController < ApplicationController
+  before_action :check_user
+  helper_method :message
+  helper_method :conversation_messages
+
+  def show
+    conversation
+  end
+
+  def conversation_messages
+    Message.where(conversation_id: @conversation.id).page params[:page]
+  end
+
+  def message
+    Message.new
+  end
+
+  private
+
+  def conversation
+    @conversation = Conversation.find(params[:id])
+  end
+
+  def check_user
+    redirect_to root_path unless conversation.boy_id = current_boy&.id || conversation.girl_id = current_girl&.id
+  end
+end
