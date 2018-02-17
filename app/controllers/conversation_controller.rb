1926b8275e2c8f3f652cbb2c3236c67714779c8a
onii4an <svyat4work@gmail.com>
2018-02-17 18:23:09 +0200

    correct conversation controller

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -2,21 +2,20 @@ class ConversationController < ApplicationController
   before_action :check_user
 
   def index
-    current_boy.update('waiting' => true) if current_boy
-    current_girl.update('waiting' => true) if current_girl
+    current_user.update('waiting' => true)
     if Boy.where(waiting: true).size > 0 && Girl.where(waiting: true).size > 0
       @offset_boy = rand(Boy.where(waiting: true).size)
       @boy_id = Boy.where(waiting: true).offset(@offset_boy).first.id
       @offset_girl = rand(Girl.where(waiting: true).size)
       @girl_id = Girl.where(waiting: true).offset(@offset_girl).first.id
       Conversation.create('boy_id' => @boy_id, 'girl_id' => @girl_id)
-      ConversationJob.perform_later('created', [@boy_id, @girl_id, Conversation.where('boy_id' => @boy_id, 'girl_id' => @girl_id).first.id])
+      ConversationJob.perform_later('create', [@boy_id, @girl_id, Conversation.where('boy_id' => @boy_id, 'girl_id' => @girl_id).last.id])
     end
   end
 
   private
 
   def check_user
-    redirect_to root_path unless current_boy || current_girl
+    redirect_to root_path unless current_user
   end
 end
