8c1d2278a961ade4d8ca003ec67e0d1f47aaf3d0
onii4an <svyat4work@gmail.com>
2018-02-20 20:48:11 +0200

    edit all user models

--- app/models/girl.rb
+++ app/models/girl.rb
@@ -1,3 +1,12 @@
 class Girl < User
   has_many :conversation
+  has_many :messages, dependent: :destroy
+
+  validates :name, presence: :true
+  validates :surname, presence: :true
+  validates :age, presence: :true
+  mount_uploader :avatar, AvatarUploader
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :messages, dependent: :destroy
-
-  validates :name, presence: :true
-  validates :surname, presence: :true
-  validates :age, presence: :true
-  # validates :sex, presence: :true
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
-  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :onliner
-  validates_presence_of   :avatar
-  validates_integrity_of  :avatar
-  validates_processing_of :avatar
 
   attr_accessor :login
   def self.find_for_database_authentication(warden_conditions)
