fe37e6077e4a4ed7e07a4d32e6f03095ef771a10
onii4an <svyat4work@gmail.com>
2018-02-26 15:32:12 +0200

    delete uniqness of conversation

--- db/migrate/20180201193932_add_uniqness_of_conversation.rb
+++ db/migrate/20180201193932_add_uniqness_of_conversation.rb
@@ -1,5 +1,5 @@
 class AddUniqnessOfConversation < ActiveRecord::Migration[5.1]
   def change
-    add_index :conversations, [:boy_id, :girl_id], unique: true
+    # add_index :conversations, [:boy_id, :girl_id], unique: true
   end
 end
