e1aaa23b13af89a0b63e62e2f1969af5106babc1
onii4an <svyat4work@gmail.com>
2018-03-05 14:07:17 +0200

    back current conversation for action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,9 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
+    identified_by :current_conversation
 
     def connect
       self.current_user = User.find_by(id: cookies[:user_id])
+      if current_user
+        if current_user.type == "Boy"
+          self.current_conversation = Conversation.where(boy_id: current_user.id)&.last
+        end
+        if current_user.type == "Girl"
+          self.current_conversation = Conversation.where(girl_id: current_user.id)&.last
+        end
+      end
       reject_unauthorized_connection unless current_user
     end
   end
--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -1,6 +1,6 @@
 class MessageChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "cv_#{current_conversation&.id}"
+    stream_from "cv_#{current_conversation.id}" if current_conversation.present?
   end
 
   def unsubscribed
