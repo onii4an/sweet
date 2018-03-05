3f19d1527bfa8380cf897c4b638b193ea45fe173
onii4an <svyat4work@gmail.com>
2018-03-05 20:02:10 +0200

    remove channels for admin

--- app/channels/message_channel.rb
+++ app/channels/message_channel.rb
@@ -4,11 +4,13 @@ class MessageChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    ActionCable.server.broadcast "user_#{current_conversation.boy_id}", action: :unsubscribed
-    ActionCable.server.broadcast "user_#{current_conversation.girl_id}", action: :unsubscribed
-    Boy.find(current_conversation.boy_id).update in_a_conversation: false
-    Girl.find(current_conversation.girl_id).update in_a_conversation: false
-    current_conversation.update active: false
+    unless current_user.type == "Admin"
+      ActionCable.server.broadcast "user_#{current_conversation.boy_id}", action: :unsubscribed
+      ActionCable.server.broadcast "user_#{current_conversation.girl_id}", action: :unsubscribed
+      Boy.find(current_conversation.boy_id).update in_a_conversation: false
+      Girl.find(current_conversation.girl_id).update in_a_conversation: false
+      current_conversation.update active: false
+    end
     # Any cleanup needed when channel is unsubscribed
   end
 end
