4b26b8c1169b4bdb7dc289f6e1ea2b3053564275
onii4an <svyat4work@gmail.com>
2018-03-05 17:53:27 +0200

    correct waiting controller

--- app/controllers/conversation_controller.rb
+++ app/controllers/conversation_controller.rb
@@ -3,21 +3,22 @@ class ConversationController < ApplicationController
 
   def index
     current_user.update('waiting' => true)
-    if Boy.where(waiting: true, in_a_conversation: false).size > 0 && Girl.where(waiting: true, in_a_conversation: false).size > 0
+    if Boy.where(waiting: true).size > 0 && Girl.where(waiting: true).size > 0
       @offset_boy = rand(Boy.where(waiting: true).size)
       @boy_id = Boy.where(waiting: true).offset(@offset_boy).first.id
       @offset_girl = rand(Girl.where(waiting: true).size)
       @girl_id = Girl.where(waiting: true).offset(@offset_girl).first.id
-      if Conversation.create('boy_id' => @boy_id, 'girl_id' => @girl_id)
-        current_conversation.boy.update('in_a_conversation'=> true, 'waiting' => false)
-        current_conversation.girl.update('in_a_conversation'=> true, 'waiting' => false)
-        ConversationJob.perform_later('create', [@boy_id, @girl_id, Conversation.where('boy_id' => @boy_id, 'girl_id' => @girl_id).last.id])
-      end
+      Conversation.create boy_id: @boy_id, girl_id: @girl_id
+      ConversationJob.perform_later('create', [@boy_id, @girl_id, Conversation.where('boy_id' => @boy_id, 'girl_id' => @girl_id, active: true).last.id])
     end
   end
 
   def leave
-    #current_conversation.destroy
+    ActionCable.server.broadcast "user_#{current_conversation.boy_id}", action: :unsubscribed
+    ActionCable.server.broadcast "user_#{current_conversation.girl_id}", action: :unsubscribed
+    Boy.find(current_conversation.boy_id).update in_a_conversation: false
+    Girl.find(current_conversation.girl_id).update in_a_conversation: false
+    current_conversation.update active: false
     redirect_to root_path
   end
 
