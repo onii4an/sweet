6f6fb79ce885cc5b163ae38c8c2161cd8412e246
Sergey <sergeyripchanskiy@gmail.com>
2018-04-03 11:49:43 +0300

    redirect to user_profile after autorization

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,10 @@ class WelcomeController < ApplicationController
   private
 
   def check_user
-    redirect_to main_boy_path if current_boy
-    redirect_to main_girl_path if current_girl
+    redirect_to users_path(current_boy.id) if current_boy
+    #redirect_to main_boy_path if current_boy
+    redirect_to users_path(current_girl.id) if current_girl
+    #redirect_to main_girl_path if current_girl
     redirect_to main_admin_path if current_admin
   end
 end
