ead59c8b574bb37ebb81f111d67561aa10412a0b
Sergey <sergeyripchanskiy@gmail.com>
2018-04-04 00:08:35 +0300

    add edit status, validates for status length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :intro, length: { maximum: 50 }
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
   devise :database_authenticatable, :registerable,
