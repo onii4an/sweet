89d41b7116249bb068ae5e5731bcf923016d98c1
onii4an <svyat4work@gmail.com>
2018-04-14 12:57:56 +0300

    add check user for ban page

--- app/controllers/ban_controller.rb
+++ app/controllers/ban_controller.rb
@@ -1,3 +1,11 @@
 class BanController < ApplicationController
+  defore_action :check_user
+
   def banned; end
+
+  private
+
+  def check_user
+    redirect_to root_path unless current_user.status == 'banned'
+  end
 end
